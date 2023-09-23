7a72ce65c1fbbfab7c83f391fbcf406eb9385b7d
Ferhat Adıbelli <ferhatadibelli9@gmail.com>
2023-09-23 11:34:05 +0300

    new

--- src/controllers/whatsAppController.js
+++ src/controllers/whatsAppController.js
@@ -1,5 +1,16 @@
 const verifyToken = (req, res) => {
-  return res.send('verfiyToken success');
+  try {
+    const accessToken = 'WEDD34343R4R545F4F45FF465G5GT';
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (token !== null && challenge !== null && token == accessToken) {
+      res.send(challenge);
+    }
+  } catch (error) {
+    res.status(400).send();
+  }
+  return res.send('verfiyToken success!!!!!');
 };
 
 const recievedMessage = (req, res) => {
